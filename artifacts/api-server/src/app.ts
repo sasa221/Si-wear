bc79f5f7d78d4ed2644180ad104b47f92c54736d
sasa221 <saleh2005mohamed@gmail.com>
2026-06-21 23:48:36 +0300

    fix vercel api nodenext build

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,10 +1,20 @@
 import express, { type NextFunction, type Request, type Response } from "express";
 import cors from "cors";
-import { pinoHttp } from "pino-http";
+import pinoHttpModule, { type HttpLogger, type Options } from "pino-http";
+import type { IncomingMessage, ServerResponse } from "node:http";
 import router from "./routes/index.js";
 import { logger } from "./lib/logger.js";
 
 type RequestWithId = Request & { id?: unknown };
+type PinoHttpFactory = <IM = IncomingMessage, SR = ServerResponse, CustomLevels extends string = never>(
+  opts?: Options<IM, SR, CustomLevels>,
+) => HttpLogger<IM, SR, CustomLevels>;
+
+const pinoHttp = (
+  typeof pinoHttpModule === "function"
+    ? pinoHttpModule
+    : (pinoHttpModule as unknown as { default?: PinoHttpFactory }).default
+) as PinoHttpFactory;
 
 export const app = express();
 
