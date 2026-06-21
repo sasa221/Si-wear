75fdf2dd59a8e0d35d88e6a471831f4531f0a00d
sasa221 <saleh2005mohamed@gmail.com>
2026-06-21 23:54:08 +0300

    fix api express app for vercel

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,23 +1,22 @@
 import express, { type NextFunction, type Request, type Response } from "express";
 import cors from "cors";
-import pinoHttpModule, { type HttpLogger, type Options } from "pino-http";
-import type { IncomingMessage, ServerResponse } from "node:http";
+import pinoHttpModule from "pino-http";
 import router from "./routes/index.js";
 import { logger } from "./lib/logger.js";
 
 type RequestWithId = Request & { id?: unknown };
-type PinoHttpFactory = <IM = IncomingMessage, SR = ServerResponse, CustomLevels extends string = never>(
-  opts?: Options<IM, SR, CustomLevels>,
-) => HttpLogger<IM, SR, CustomLevels>;
 
 const pinoHttp = (
   typeof pinoHttpModule === "function"
     ? pinoHttpModule
-    : (pinoHttpModule as unknown as { default?: PinoHttpFactory }).default
-) as PinoHttpFactory;
+    : (pinoHttpModule as any).default
+);
 
 export const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
   pinoHttp({
     logger,
@@ -37,9 +36,6 @@ app.use(
     },
   }),
 );
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router);
 
